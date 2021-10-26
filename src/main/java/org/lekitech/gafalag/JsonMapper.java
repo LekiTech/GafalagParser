259bf57b941838fe5ff071a60659073b1ea05ad9
eskendarov <envereskendarov@gmail.com>
2021-10-26 10:02:44 +0300

    added out

--- src/main/java/org/lekitech/gafalag/JsonMapper.java
+++ src/main/java/org/lekitech/gafalag/JsonMapper.java
@@ -1,14 +1,18 @@
 package org.lekitech.gafalag;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.lekitech.gafalag.structuredmodels.Page;
-import org.lekitech.gafalag.structuredmodels.Text;
+import org.lekitech.gafalag.dictionarymodels.EntryWord;
+import org.lekitech.gafalag.jsonmodels.Page;
+import org.lekitech.gafalag.jsonmodels.Text;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * Date: 15.10.2021
@@ -20,17 +24,112 @@ import java.util.function.Consumer;
  */
 public class JsonMapper {
 
-    public static void main(String[] args) throws IOException {
-        final ObjectMapper mapper = new ObjectMapper();
-        final File file = new File("src/main/resources/dictionary.json");
-        final List<Page> pages = mapper.readerFor(new TypeReference<List<Page>>() {
-        }).readValue(file);
-        final Consumer<Text> print = text -> {
-            if (text.isNewWord()) {
-                System.out.println();
+    private final StringBuilder leftPart;
+    private final StringBuilder rightPart;
+    private final List<EntryWord> dictionary;
+    private final ObjectMapper mapper;
+    private EntryWord entryWord;
+    private List<Page> pages;
+
+    public JsonMapper(String src) {
+        leftPart = new StringBuilder();
+        rightPart = new StringBuilder();
+        dictionary = new ArrayList<>();
+        mapper = new ObjectMapper();
+        entryWord = new EntryWord();
+        try {
+            pages = mapper.readerFor(new TypeReference<List<Page>>() {
+            }).readValue(new File(src));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void parseJSON() {
+        var texts = pages.stream()
+                .flatMap(page -> page.getTextOfPage().stream())
+                .collect(Collectors.toList());
+        for (int i = 0, textItSize = texts.size(); i < textItSize; i++) {
+            final Text current = texts.get(i);
+            final Text next = i == textItSize - 1 ? current : texts.get(i + 1);
+            if (current.isValidZone()) {
+                if (current.isHeader()) {
+                    leftPart.append(current.value());
+                    leftPart.append(current.value().endsWith(" ") ? "" : " ");
+                } else {
+                    rightPart.append(current.value()).append(" ");
+                    if (current.isIdiom() && next.isDefault()) {
+                        rightPart.append(": ");
+                    }
+                }
             }
-            System.out.print(text.get());
-        };
-        pages.stream().limit(4).flatMap(page -> page.getTextData().stream()).forEach(print);
+            if (!current.isNewEntry() && (next.isNewEntry() || i == textItSize - 1)) {
+                if (leftPart.length() != 0) {
+                    splitHeadAndInflection(checkPunc(leftPart.toString()));
+                    splitDefinitions(checkPunc(rightPart.toString()));
+                    addEntry(entryWord);
+                    leftPart.setLength(0);
+                    rightPart.setLength(0);
+                }
+            }
+        }
+    }
+
+    public void writeJSON(String target) {
+        parseJSON();
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValues(new File(target)).write(dictionary);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printJSON() {
+        parseJSON();
+        try {
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(dictionary));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void splitHeadAndInflection(String value) {
+        if (value.matches(".*\\(.*\\).*")) {
+            final String[] s = value.split("[()]");
+            entryWord.setHead(s[0].trim().toUpperCase());
+            entryWord.setInflection(String.format("(%s)", s[1].trim()));
+        } else {
+            entryWord.setHead(value.trim().toUpperCase());
+        }
+    }
+
+    private void splitDefinitions(String rightPart) {
+        var defs = Arrays
+                .stream(rightPart.trim().split("[1-9]+[.)]|;"))
+                .filter(s -> !s.isBlank())
+                .map(String::trim)
+                .collect(Collectors.toList());
+        entryWord.setDefinitions(defs);
+    }
+
+    private void addEntry(final EntryWord entry) {
+        dictionary.add(entry);
+        entryWord = new EntryWord();
+    }
+
+    private String checkPunc(String value) {
+        return value
+                .replace(" ,", ",")
+                .replace(" .", ".")
+                .replace(" !", "!")
+                .replace(" ?", "?")
+                .replace(" ;", ";")
+                .replace(" :", ":")
+                .replace("( ", "(")
+                .replace(" )", ")")
+                .replace("  ", " ")
+                .replace(" I ", "I")
+                .replace("- ", "-")
+                .replace(" -", "-");
     }
 }
--- src/main/java/org/lekitech/gafalag/jsonmodels/Text.java
+++ src/main/java/org/lekitech/gafalag/jsonmodels/Text.java
@@ -1,28 +1,70 @@
 package org.lekitech.gafalag.jsonmodels;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
-import java.util.List;
+import java.util.LinkedList;
+
+import static org.lekitech.gafalag.jsonmodels.Color.BLACK;
+import static org.lekitech.gafalag.jsonmodels.Color.BLUE;
 
 /**
- * Date: 07.10.2021
+ * Date: 15.10.2021
  * Project: GafalagParser
  * Class: Text
  *
  * @author Enver Eskendarov (envereskendarov@gmail.com)
  * @version 1.0
  */
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = {"A"})
 public class Text {
 
     @JsonProperty("oc")
-    public String dictTextColor;
-    public double x;
-    public double y;
-    public double w;
-    public double sw;
-    public int clr;
-    @JsonProperty("A")
-    public String a;
+    private String color;
+    private double x, y, w, sw, clr;
     @JsonProperty("R")
-    public List<R> r;
+    private LinkedList<R> r;
+
+    public boolean isHeader() {
+        return getColor().equals(BLUE) && isBold();
+    }
+
+    public boolean isIdiom() {
+        return getColor().equals(BLACK) && isBold();
+    }
+
+    public boolean isDefault() {
+        return getFontSize() == 13.02 && !isBold();
+    }
+
+    public boolean isNewEntry() {
+        return getX() == 2.337 && isHeader();
+    }
+
+    /**
+     * @return Возвращает область страницы без верхнего колонтитула.
+     */
+    public boolean isValidZone() {
+        return getY() > 1.012;
+    }
+
+    public String value() {
+        return isValidZone() ? r.get(0).getText() : null;
+    }
+
+    public Color getColor() {
+        return Color.ofHex(color);
+    }
+
+    public double getFontSize() {
+        return r.get(0).getFontType().get(1);
+    }
+
+    public boolean isBold() {
+        return r.get(0).getFontType().get(2) == 1;
+    }
 }
