4b189601289029b9eae2156bb26ccf6019816eeb
eskendarov <envereskendarov@gmail.com>
2021-11-10 18:55:42 +0300

    changed text formatting

--- src/main/java/org/lekitech/gafalag/Mapper.java
+++ src/main/java/org/lekitech/gafalag/Mapper.java
@@ -5,13 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import org.lekitech.gafalag.dictionarymodels.Article;
+import org.lekitech.gafalag.jsonmodels.Color;
 import org.lekitech.gafalag.jsonmodels.Page;
-import org.lekitech.gafalag.jsonmodels.Token;
+import org.lekitech.gafalag.jsonmodels.Text;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -33,6 +36,10 @@ public class Mapper extends ObjectMapper {
     final StringBuilder inf = new StringBuilder();
     final StringBuilder def = new StringBuilder();
 
+    // valid page area
+    final Predicate<Text> valid = text -> text.getY() > 1.012d;
+    final Predicate<Text> startArticle = text -> text.getX() < 2.65 && text.colorIs(Color.BLUE);
+
     public Mapper(String srcFile) {
         this.srcFile = srcFile;
     }
@@ -41,74 +48,96 @@ public class Mapper extends ObjectMapper {
     public Mapper init() {
         final List<Page> pages = readerFor(new TypeReference<List<Page>>() {
         }).readValue(new File(srcFile));
-        final List<Token> tokens = pages.stream()
+        final List<Text> tokens = pages.stream()
                 .flatMap(page -> page.getTextBlocks().stream())
-                .map(Token::new).filter(Token::isValid).collect(Collectors.toList());
+                .filter(valid)
+                .collect(Collectors.toList());
+        FileWriter writer = new FileWriter("res.txt");
         for (int i = 0, size = tokens.size(); i < size; i++) {
-            final Token current = tokens.get(i);
-            final Token previous = (i == 0 || current.isStartNewArticle()) ? null : tokens.get(i - 1);
-            final Token next = ((i == size - 1) || tokens.get(i + 1).isStartNewArticle()) ? null : tokens.get(i + 1);
+
+            final Text current = tokens.get(i);
+            final Text previous = (i == 0 || startArticle.test(current)) ? null : tokens.get(i - 1);
+            final Text next = ((i == size - 1) || startArticle.test(tokens.get(i + 1))) ? null : tokens.get(i + 1);
+            if (current.getW() == 0.483) {
+                writer.write(current.getText());
+            }
             switch (current.getColor()) {
-                case BLUE -> setExprAndInfSplit(current);
-                case GREEN -> setTagsAndAppend(previous, current, next, "<", ">");
-                case BLACK -> setTagsAndAppend(previous, current, next, "{", "}");
-                case BROWN -> setTagsAndAppend(previous, current, next, "", "");
+                case BLUE -> checkSpaceAndAddHead(current, next);
+                case GREEN -> setTagsAndSpaceAndAppend(previous, current, next, "<", ">");
+                case BLACK -> setTagsAndSpaceAndAppend(previous, current, next, "{", "}");
+                case BROWN -> setTagsAndSpaceAndAppend(previous, current, next, "", "");
             }
-            if (next == null) {
-                dictionary.add(setNewArticle());
-                exp.setLength(0);
-                inf.setLength(0);
-                def.setLength(0);
-            }
+            addArticleIfNotExist(next);
         }
+        writer.flush();
         return this;
     }
 
-    private void setExprAndInfSplit(Token token) {
-        if (token.getText().contains("(")) {
-            var split = token.getText().split("(\\()", 2);
-            exp.append(split[0]);
-            inf.append("(").append(split[1]);
-        } else if (inf.length() == 0) {
-            exp.append(token.getText());
+    private void checkSpaceAndAddHead(Text current, Text next) {
+        if (current.getText().matches(".*[кптцч]") && next != null && next.getText().startsWith("I")
+                || current.getText().startsWith("I") && next != null && !next.getText().startsWith("I")
+                || current.getText().contains("-") || next != null && next.getText().contains("-")
+                || current.getText().contains("[") || next != null && next.getText().contains("]")
+                || next != null && next.getText().contains(".") || next != null && next.getText().contains(",")
+        ) {
+            exp.append(current.getText());
         } else {
-            inf.append(token.getText()).append(" ");
+            exp.append(current.getText()).append(" ");
         }
     }
 
-    private Article setNewArticle() {
-        return new Article(
-                exp.toString().trim(),
-                inf.length() != 0 ? checkPartsOfSpeech(inf.toString()) : null,
-                Stream.of(checkPartsOfSpeech(def.toString()).split("(?=\\d\\.)")
-                ).filter(s -> !s.isEmpty() && !s.isBlank()).map(String::trim).collect(Collectors.toList())
-        );
+    private void expSplit() {
+        var expInf = exp.toString().split("[()]");
+        if (expInf.length > 1) {
+            exp.setLength(0);
+            exp.append(expInf[0]);
+            inf.append(expInf[1]);
+        }
+        var expDef = exp.toString().split(":");
+        if (expDef.length > 1) {
+            exp.setLength(0);
+            exp.append(expDef[0]);
+            def.insert(0, "{" + expDef[1].trim() + "} ");
+        }
     }
 
-    private void setTagsAndAppend(Token previous, Token current, Token next, String firstTag, String lastTag) {
+    private void addArticleIfNotExist(Text next) {
+        if (next == null) {
+            expSplit();
+            dictionary.add(new Article(
+                    (exp.toString().replaceAll("\\s{2,}", " ")).trim(),
+                    inf.length() != 0 ? checkPartsOfSpeechAndPunc(inf.toString()).trim() : null,
+                    Stream.of(checkPartsOfSpeechAndPunc(def.toString()).split("(?=\\d\\.)")
+                    ).filter(s -> !s.isEmpty() && !s.isBlank()).map(String::trim).collect(Collectors.toList())
+            ));
+            exp.setLength(0);
+            inf.setLength(0);
+            def.setLength(0);
+        }
+    }
+
+    private void setTagsAndSpaceAndAppend(Text previous, Text current, Text next, String firstTag, String lastTag) {
         if (previous == null || current.getColor() != previous.getColor()) {
-            def.append(firstTag);
+            def.append(" ").append(firstTag);
         }
         def.append(current.getText());
-        if (!current.getText().endsWith(" ")) {
+        if (!current.getText().endsWith(" ") && current.swEquals(next) && current.colorIs(next)) {
             def.append(" ");
         }
         if (next == null || current.getColor() != next.getColor()) {
-            def.append(lastTag).append(" ");
+            def.append(lastTag);
         }
     }
 
-    private String checkPartsOfSpeech(String text) {
+    private String checkPartsOfSpeechAndPunc(String text) {
         return text
-                .replaceAll("\\s?([\\-I/])\\s?", "$1")
-                .replaceAll("\\s?([!,.:;?])", "$1")
-                .replaceAll("([<{(])\\s+", "$1")
-                .replaceAll("\\s+([)}>])", "$1 ")
-                .replaceAll("([)}>])\\s([;.,:])", "$1$2")
-                .replaceAll("([\\-I])\\s+", "$1")
-                .replaceAll("\\s?Ø\\s?", "")
-                .replaceAll(",-", ", -")
-                .replaceAll("\\s+", " ")
+                .replaceAll("([({<\\[])\\s", "$1")
+                .replaceAll("\\s([,:;.>})\\]])", "$1")
+                .replaceAll("([\\p{IsCyrillic}\\w[^\\s{<]])(\\()", "$1 $2")
+                .replaceAll("(\\.)([\\p{IsCyrillic}\\w[^\\s}>)]])", "$1 $2")
+                .replaceAll("\\s{2,}", " ")
+                .replaceAll("(,)(-)", "$1 $2")
+                .replaceAll("Ø", "♦")
                 .trim();
     }
 }
