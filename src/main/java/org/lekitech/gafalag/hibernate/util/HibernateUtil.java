a2905cd1dff8d7f1d73aaaf705f0d6c5efb16382
eskendarov <envereskendarov@gmail.com>
2021-11-12 17:08:20 +0300

    [GAF-2] added Hibernate configuration

---
+++ src/main/java/org/lekitech/gafalag/hibernate/util/HibernateUtil.java
@@ -0,0 +1,48 @@
+package org.lekitech.gafalag.hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * Date: 12.11.2021
+ * Project: GafalagParser
+ * Class: HibernateUtil
+ *
+ * @author Enver Eskendarov (envereskendarov@gmail.com)
+ * @version 1.0
+ */
+public class HibernateUtil {
+
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                // Create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
+                // Create MetadataSources
+                final MetadataSources sources = new MetadataSources(registry);
+                // Create Metadata
+                final Metadata metadata = sources.getMetadataBuilder().build();
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
