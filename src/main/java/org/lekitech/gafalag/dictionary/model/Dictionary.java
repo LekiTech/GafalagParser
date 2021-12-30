c733a0b1366211dab5940c105aacebb6a0c0818b
eskendarov <envereskendarov@gmail.com>
2021-12-31 00:23:31 +0300

    bug fixing

--- src/main/java/org/lekitech/gafalag/dictionary/model/Dictionary.java
+++ src/main/java/org/lekitech/gafalag/dictionary/model/Dictionary.java
@@ -3,6 +3,7 @@ package org.lekitech.gafalag.dictionary.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
+import org.lekitech.gafalag.util.Resource;
 
 import java.util.List;
 
@@ -14,15 +15,15 @@ import static org.lekitech.gafalag.util.Resource.*;
 @JsonPropertyOrder({
         "name",
         "url",
-        "sourceLangIso3",
-        "targetLangIso3",
+        "expressionLanguageIso3",
+        "definitionLanguageIso3",
         "dictionary"
 })
 public class Dictionary {
 
     private final String name = DICTIONARY_NAME;
     private final String url = DICTIONARY_URL;
-    private final String sourceLangIso3 = SOURCE_LANG_ISO_3;
-    private final String targetLangIso3 = TARGET_LANG_ISO_3;
+    private final String expressionLanguageIso3 = EXPRESSION_LANGUAGE_ISO_3;
+    private final String definitionLanguageIso3 = DEFINITION_LANGUAGE_ISO_3;
     private final @NonNull List<Article> dictionary;
 }
--- src/main/java/org/lekitech/gafalag/util/Resource.java
+++ src/main/java/org/lekitech/gafalag/util/Resource.java
@@ -12,8 +12,8 @@ public class Resource {
     public static final String TARGET_JSON_PATH = res.getString("target.path");
     public static final String DICTIONARY_NAME = res.getString("dictionary.name");
     public static final String DICTIONARY_URL = res.getString("dictionary.url");
-    public static final String SOURCE_LANG_ISO_3 = res.getString("source.lang.iso3");
-    public static final String TARGET_LANG_ISO_3 = res.getString("target.lang.iso3");
+    public static final String EXPRESSION_LANGUAGE_ISO_3 = res.getString("expression.language.iso3");
+    public static final String DEFINITION_LANGUAGE_ISO_3 = res.getString("definition.language.iso3");
     public static int START_PAGE;
     public static int END_PAGE;
 
